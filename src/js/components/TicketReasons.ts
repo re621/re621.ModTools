9616d41c3ad50ddd42612dab40da9126f471eacd
Justin Morris <jmortiger@gmail.com>
2025-10-19 11:16:55 -0400

    FIxed events not being preserved on add/remove/reset buttons after buttons are rebuilt

--- src/js/components/TicketReasons.ts
+++ src/js/components/TicketReasons.ts
@@ -3,8 +3,7 @@ import { DialogForm } from "../models/structure/DialogForm";
 import Component from "./Component";
 
 /**
- * @todo Add button removal mode.
- * @todo Add button reset mode.
+ * @todo Add red styling in removal mode.
  */
 export default class TicketReasons extends Component {
 
@@ -31,12 +30,12 @@ export default class TicketReasons extends Component {
 			{ name: "Reviewed", text: "Reviewed, thank you." },
 			{ name: "NAT", text: "Reviewed, no action taken." },
 			{ name: "Closed", text: "Ticket closed." },
-			{ name: "Old", text: "That comment is from N years ago.\nWe do not punish people for comments older than 6 months." },
+			{ name: "Old", text: "Thank you for your report, but that comment is from N years ago, & we do not punish people for comments older than 6 months." },
 			{ name: "Reply", text: "I believe that you tried to reply to a comment, but reported it instead.\nPlease, be more careful in the future." },
-			{ name: "Already", text: "User already received a record for that message." },
-			{ name: "Banned", text: "This user is already banned." },
-			{ name: "Blacklist", text: "If you find the contents of that post objectionable, [[e621:blacklist|blacklist]] it." },
-			{ name: "Takedown", text: "Artists and character owners may request a takedown here: https://e621.net/static/takedown.\nWe do not accept third party takedowns." }
+			{ name: "Already", text: "Thank you for your report, but this user already received a record for that message." },
+			{ name: "Banned", text: "Thank you for your report, but this user is already banned." },
+			{ name: "Blacklist", text: "Thank you for your report, but this post's content does not violate our \"\":[/help/uploading_guidelines].\nIf you find the contents of the post objectionable, we'd ask you to add the relevant tags (or the post's id itself) to your \"blacklist\":[/help/blacklist]." },
+			{ name: "Takedown", text: "Artists and character owners may request a takedown \"here\":/static/takedown.\nWe do not accept third party takedowns." }
 		];
 	}
 	public Settings = {
@@ -49,51 +48,52 @@ export default class TicketReasons extends Component {
 		this.input = $("textarea[name='ticket[response]']");
 
 		const wrapper = this.input.parents("td");
+		wrapper.on("click", "button", (e) => {
+			this.input.val("Would you look at that!");
+		});
 		this.container = $("<div>")
 			.addClass("ticket-responses")
 			.prependTo(wrapper)
 			.on("click", "button", (event) => {
-				event.stopPropagation();
-				console.log(`Wrapper caught click event; inRemovalState = ${this.inRemovalState}`);
-
-				try { throw new Error(""); } catch (error) { }
 				const button = $(event.currentTarget);
 				if (!this.inRemovalState) {
 					this.input.val(button.attr("text"));
-				} else {
+				} else if (confirm(`Are you sure you want to delete this button?\n\n\tNAME: ${event.target.name}\n\tTEXT: "${event.target.getAttribute("text")}"`)) {
 					let temp = this.Settings.buttons;
 					temp = temp.filter((e) => e.name !== event.target.name || e.text !== event.target.getAttribute("text"));
-					// temp = temp.filter((e, i) => i.toString() !== button.attr("index"));
 					this.Settings.buttons = temp;
-					this.rebuildButtons();
+					this.buildButtons();
 				}
 
+				// Stop propagation & prevent default.
 				return false;
 			});
 
-		this.rebuildButtons();
+		this.buildButtons();
 
 		return;
 	}
 
-	private rebuildButtons(): void {
+	private buildButtons(): void {
+		// Preserve buttons; doesn't preserve their events.
 		this.container.detach("div.ticket-responses > add-ticket-response-button, div.ticket-responses > remove-ticket-response-button");
 		this.container.html("");
 
-		this.Settings.buttons.forEach((button, i) => {
+		for (const button of this.Settings.buttons) {
 			$("<button>")
 				.attr({
 					class: "ticket-response-button",
-					index: i,
 					name: button.name,
 					text: button.text,
 				})
 				.text(button.name)
 				.appendTo(this.container);
-		});
+		}
 		this.addButton.appendTo(this.container);
 		this.removeButton.appendTo(this.container);
 		this.resetButton.appendTo(this.container);
+		// (Re)bind the events
+		this.bindButtons();
 	}
 
 	private get addButton() {
@@ -103,31 +103,7 @@ export default class TicketReasons extends Component {
 				name: "Add Custom Button",
 				text: "...You shouldn't see this...",
 			})
-			.text("+")
-			.on("click", (e) => {
-				e.preventDefault();
-				e.stopImmediatePropagation();
-				try { throw new Error(""); } catch (error) { }
-				DialogForm.getRequestedInput(
-					[
-						$('<label for="new-button-name">Name</label>'),
-						$('<input id="new-button-name" name="new-button-name" required min=1 />'),
-						$('<br />'),
-						$('<label for="new-button-text">Text</label>'),
-						$('<textarea id="new-button-text" name="new-button-text" required min=1></textarea>'),
-					],
-					"Add Button...",
-					(e: FormData) => {
-						const temp = this.Settings.buttons;
-						temp.push({
-							name: e.get("new-button-name").toString(),
-							text: e.get("new-button-text").toString(),
-						});
-						this.Settings.buttons = temp;
-						this.rebuildButtons();
-					},
-				);
-			});
+			.text("+");
 	}
 
 	private get removeButton() {
@@ -138,26 +114,7 @@ export default class TicketReasons extends Component {
 				text: "...You shouldn't see this...",
 				title: "Enter button removal mode.",
 			})
-			.text("-")
-			.on("click", (e) => {
-				e.preventDefault();
-				e.stopImmediatePropagation();
-
-				// console.log(`toggling state from ${this.inRemovalState} to ${!this.inRemovalState}`);
-				// try { throw new Error(""); } catch (error) { }
-				if (this.toggleState()) {
-					e.target.innerText = "x";
-					e.target.title = "Exit button removal mode.";
-					this.addButton.attr("disabled", "true");
-				} else {
-					e.target.innerText = "-";
-					e.target.title = "Enter button removal mode.";
-					this.addButton.removeAttr("disabled");
-				}
-
-				// // Stop propagation & prevent default.
-				// return false;
-			});
+			.text("-");
 	}
 
 	private get resetButton() {
@@ -168,19 +125,57 @@ export default class TicketReasons extends Component {
 				text: "...You shouldn't see this...",
 				title: "Reset buttons to their default values.",
 			})
-			.text("Reset")
-			.on("click", (e) => {
-				e.preventDefault();
-				e.stopImmediatePropagation();
+			.text("Reset");
+	}
 
-				if (confirm("Are you sure you want to reset the buttons to the defaults?\n\nThis will permanently remove your custom buttons.")) {
-					this.Settings.buttons = TicketReasons.createDefaultSettingsButtons();
-					this.rebuildButtons();
-				}
+	private bindButtons() {
+		this.addButton.on("click", (e) => {
+			DialogForm.getRequestedInput(
+				[
+					$('<label for="new-button-name">Name</label>'),
+					$('<input id="new-button-name" name="new-button-name" required min=1 />'),
+					$('<br />'),
+					$('<label for="new-button-text">Text</label>'),
+					$('<textarea id="new-button-text" name="new-button-text" required min=1></textarea>'),
+				],
+				"Add Button...",
+				(e: FormData) => {
+					const temp = this.Settings.buttons;
+					temp.push({
+						name: e.get("new-button-name").toString(),
+						text: e.get("new-button-text").toString(),
+					});
+					this.Settings.buttons = temp;
+					this.buildButtons();
+				},
+			);
 
-				// // Stop propagation & prevent default.
-				// return false;
-			});
+			// Stop propagation & prevent default.
+			return false;
+		});
+		this.removeButton.on("click", (e) => {
+			if (this.toggleState()) {
+				e.target.innerText = "x";
+				e.target.title = "Exit button removal mode.";
+				this.addButton.attr("disabled", "true");
+			} else {
+				e.target.innerText = "-";
+				e.target.title = "Enter button removal mode.";
+				this.addButton.removeAttr("disabled");
+			}
+
+			// Stop propagation & prevent default.
+			return false;
+		});
+		this.resetButton.on("click", (e) => {
+			if (confirm("Are you sure you want to reset the buttons to the defaults?\n\nThis will permanently remove your custom buttons.")) {
+				this.Settings.buttons = TicketReasons.createDefaultSettingsButtons();
+				this.buildButtons();
+			}
+
+			// Stop propagation & prevent default.
+			return false;
+		});
 	}
 
 	/**
