794f4274408f5d21d21329487354f922c210822e
jmortiger <jmortiger@gmail.com>
2025-10-23 13:35:39 -0400

    [Ticket] Custom ticket response buttons (#2)
    Lets users add persistent custom ticket response buttons & remove any unwanted ticket response buttons.

--- src/js/components/Component.ts
+++ src/js/components/Component.ts
@@ -1,7 +1,9 @@
 import REMT from "../../REMT";
 import XM from "../models/api/XM";
+import XMStorage from "../models/api/XMStorage";
 import KeybindManager, { Keybind, ResponseFunction } from "../models/data/Keybinds";
 import Page from "../models/data/Page";
+import Debug from "../models/Debug";
 import PageObserver from "../models/structure/PageObserver";
 import ErrorHandler from "../utilities/ErrorHandler";
 import Util from "../utilities/Util";
@@ -57,7 +59,16 @@ export default class Component {
         // Load in the saved settings values
         for (const [key, defaultValue] of Object.entries(settings || this.Settings)) {
 
+			// console.log(`Retrieving ${this.name}.${key} (Default value: ${defaultValue}) from storage...`);
+			console.log(`Retrieving ${this.name}.${key} from storage...`);
+			console.log(`\tDefault value: ${defaultValue}`);
+			console.log(defaultValue);
             const savedValue = XM.Storage.getValue(this.name + "." + key, defaultValue);
+			// console.log(`Loaded ${savedValue} (Default value: ${defaultValue}) from storage for ${this.name}.${key}.`);
+			console.log(`Loaded ${this.name}.${key} from storage.`);
+			console.log(savedValue);
+			console.log(`\tDefault value: ${defaultValue}`);
+			console.log(defaultValue);
             this.SettingsCache[key] = savedValue;
             this.SettingsDefaults[key] = defaultValue;
             delete this.Settings[key];
@@ -65,18 +76,29 @@ export default class Component {
             // Define custom setters and getters
             Object.defineProperty(this.Settings, key, {
                 get: () => {
-                    // Debug.log("- fetching", passedThis.name + "." + key);
+                    Debug.log("- fetching", this.name + "." + key);
+					console.log(`Fetching ${this.name}.${key}...`);
+					console.log(this.SettingsCache[key]);
                     return this.SettingsCache[key];
                 },
                 set: (newValue) => {
-                    // Debug.log("- setting", passedThis.name + "." + key, newValue);
+                    Debug.log("- setting", this.name + "." + key, newValue);
+					console.log(`Setting ${this.name}.${key}...\nDesired:`);
+					console.log(newValue);
+					console.log(`Before:`);
+					console.log(this.SettingsCache[key]);
                     if (JSON.stringify(newValue) == JSON.stringify(defaultValue)) {
                         this.SettingsCache[key] = defaultValue;
                         XM.Storage.deleteValue(this.name + "." + key);
                     } else {
                         this.SettingsCache[key] = newValue;
-                        XM.Storage.setValue(this.name + "." + key, newValue);
+                        XM.Storage.setValueAsync(this.name + "." + key, newValue).then((e) => {
+							console.log(`Stored:`);
+							console.log(GM_getValue(this.name + "." + key, "None"));
+						});
                     }
+					console.log(`Stored:`);
+					console.log(XM.Storage.getValue(this.name + "." + key, "None"));
                 }
             })
 
--- src/js/components/TicketReasons.ts
+++ src/js/components/TicketReasons.ts
@@ -1,69 +1,193 @@
 import { PageDefinition } from "../models/data/Page";
+import { DialogForm } from "../models/structure/DialogForm";
 import Component from "./Component";
 
+/**
+ * @todo Add red styling in removal mode.
+ */
 export default class TicketReasons extends Component {
 
-    private input: JQuery<HTMLElement>;
-    private container: JQuery<HTMLElement>;
+	private input: JQuery<HTMLElement>;
+	private _addButton?: JQuery<HTMLElement>;
+	private _removeButton?: JQuery<HTMLElement>;
+	private _resetButton?: JQuery<HTMLElement>;
+	private container: JQuery<HTMLElement>;
+	private inRemovalState = false;
+	/** @returns {boolean} The resultant state. */
+	private toggleState(): boolean { return this.inRemovalState = !this.inRemovalState; }
 
-    public constructor() {
-        super({
-            constraint: PageDefinition.tickets.view,
-            waitForDOM: "textarea[name='ticket[response]']",
-            dependencies: ["TicketData"],
-        });
-    }
+	public constructor() {
+		super({
+			constraint: PageDefinition.tickets.view,
+			waitForDOM: "textarea[name='ticket[response]']",
+			dependencies: ["TicketData"],
+		});
+	}
 
-    public Settings = {
-        enabled: true,
-        buttons: [
-            { name: "Handled", text: "Handled, thank you." },
-            { name: "Reviewed", text: "Reviewed, thank you." },
-            { name: "NAT", text: "Reviewed, no action taken." },
-            { name: "Closed", text: "Ticket closed." },
-            { name: "Old", text: "That comment is from N years ago.\nWe do not punish people for comments older than 6 months." },
-            { name: "Reply", text: "I believe that you tried to reply to a comment, but reported it instead.\nPlease, be more careful in the future." },
-            { name: "Already", text: "User already received a record for that message." },
-            { name: "Banned", text: "This user is already banned." },
-            { name: "Blacklist", text: "If you find the contents of that post objectionable, [[e621:blacklist|blacklist]] it." },
-            { name: "Takedown", text: "Artists and character owners may request a takedown here: https://e621.net/static/takedown.\nWe do not accept third party takedowns." }
-        ],
-    }
+	static readonly flagText = "Thank you for the report. However, when a post is not suited for our site, a [i]\"flag\":[/help/flag_for_deletion][/i] is the correct remedy, as the moderators who handle reports can't delete posts (that's our janitors' job). [i]Reports[/i] are for when the [i]conduct[/i] relating to the post is disallowed (e.g. tag warring, removal of valid sources, uploaded with not enough tags, etc.), not for when the post's [i]content[/i] itself is disallowed.\n\n"
+	static readonly flagTextEnd = ", but for future reference, you can flag posts by selecting `Flag` instead of `Report` in the sidebar. If you have trouble remembering, selecting the appropriate reason on either page will redirect you to the correct mechanism if the current one is incorrect."
+	private static createDefaultSettingsButtons() {
+		return [
+			{ name: "Handled", text: "This ticket has been handled, thank you!" },
+			{ name: "Reviewed", text: "This ticket has been reviewed, thank you!" },
+			{ name: "NAT", text: `Thank you for the heads-up! We've reviewed the ticket and completed our investigation into the matter; however, we've decided it does not warrant action at this time.` },
+			{ name: "Closed", text: "This ticket has been closed." },
+			{ name: "Old (NAT)", text: "Thank you for your report, but that comment is from N years ago, & we do not punish people for comments older than 6 months." },
+			{ name: "Old (Hide)", text: "Thank you for your report, but that comment is from N years ago, & we do not punish people for comments older than 6 months. We've removed the comment." },
+			{ name: "Reply", text: "I believe that you tried to reply to a comment, but reported it instead.\nPlease, be more careful in the future." },
+			{ name: "Already (Rec)", text: "Thank you for your report, but this user has already received a record for this matter." },
+			{ name: "Already (Ban)", text: "Thank you for your report, but this user is already banned." },
+			{ name: "Blacklist", text: "Thank you for your report, but this post's content does not violate our \"\":[/help/uploading_guidelines].\nIf you find the contents of the post objectionable, we'd ask you to add the relevant tags (or the post's id itself) to your \"blacklist\":[/help/blacklist]." },
+			{ name: "Takedown", text: "Thank you for your report, but this matter needs to be handled via a takedown request.\nArtists and character owners may request a takedown \"here\":/static/takedown.\nWe do not accept third party takedowns." },
+			{ name: "DMed", text: "Thank you for your report, we've discussed the matter with them." },
+			{ name: "Flag (Del)", text: `${TicketReasons.flagText}The post has already been deleted by our janitors${TicketReasons.flagTextEnd}` },
+			{ name: "Flag (Flag)", text: `${TicketReasons.flagText}The post has already been flagged${TicketReasons.flagTextEnd}` },
+		];
+	}
+	public Settings = {
+		enabled: true,
+		buttons: TicketReasons.createDefaultSettingsButtons(),
+	};
 
-    protected create(): Promise<void> {
+	protected create(): Promise<void> {
+		TicketReasons.ensureModalContainer();
+		this.input = $("textarea[name='ticket[response]']");
 
-        this.input = $("textarea[name='ticket[response]']");
+		const wrapper = this.input.parents("td");
+		this.container = $("<div>")
+			.addClass("ticket-responses")
+			.prependTo(wrapper)
+			.on("click", "button", (event) => {
+				const button = $(event.currentTarget);
+				if (!this.inRemovalState) {
+					this.input.val(button.attr("text"));
+				} else if (confirm(`Are you sure you want to delete this button?\n\n\tNAME: ${event.target.name}\n\tTEXT: "${event.target.getAttribute("text")}"`)) {
+					let temp = this.Settings.buttons;
+					temp = temp.filter((e) => e.name !== event.target.name || e.text !== event.target.getAttribute("text"));
+					this.Settings.buttons = temp;
+					this.buildButtons();
+				}
 
-        const wrapper = this.input.parents("td");
-        this.container = $("<div>")
-            .addClass("ticket-responses")
-            .prependTo(wrapper)
-            .on("click", "button", (event) => {
-                event.stopPropagation();
+				// Stop propagation & prevent default.
+				return false;
+			});
 
-                const button = $(event.currentTarget);
-                this.input.val(button.attr("text"));
+		this.buildButtons();
 
-                return false;
-            });
+		return;
+	}
 
-        this.rebuildButtons();
+	private buildButtons(): void {
+		// Preserve buttons; doesn't preserve their events.
+		this.container.detach("div.ticket-responses > add-ticket-response-button, div.ticket-responses > remove-ticket-response-button");
+		this.container.html("");
 
-        return;
-    }
+		for (const button of this.Settings.buttons) {
+			$("<button>")
+				.attr({
+					class: "ticket-response-button",
+					name: button.name,
+					text: button.text,
+				})
+				.text(button.name)
+				.appendTo(this.container);
+		}
+		this.addButton.appendTo(this.container);
+		this.removeButton.appendTo(this.container);
+		this.resetButton.appendTo(this.container);
+		// (Re)bind the events
+		this.bindButtons();
+	}
 
-    private rebuildButtons(): void {
-        this.container.html("");
+	private get addButton() {
+		return this._addButton ||= $("<button>")
+			.attr({
+				class: "add-ticket-response-button",
+				name: "Add Custom Button",
+				text: "...You shouldn't see this...",
+			})
+			.text("+");
+	}
 
-        for (const button of this.Settings.buttons) {
-            $("<button>")
-                .attr({
-                    name: button.name,
-                    text: button.text,
-                })
-                .text(button.name)
-                .appendTo(this.container);
-        }
-    }
+	private get removeButton() {
+		return this._removeButton ||= $("<button>")
+			.attr({
+				class: "remove-ticket-response-button",
+				name: "Remove Buttons",
+				text: "...You shouldn't see this...",
+				title: "Enter button removal mode.",
+			})
+			.text("-");
+	}
 
+	private get resetButton() {
+		return this._resetButton ||= $("<button>")
+			.attr({
+				class: "reset-ticket-response-button",
+				name: "Reset Buttons",
+				text: "...You shouldn't see this...",
+				title: "Reset buttons to their default values.",
+			})
+			.text("Reset");
+	}
+
+	private bindButtons() {
+		this.addButton.on("click", () => {
+			DialogForm.getRequestedInput(
+				[
+					$('<label for="new-button-name">Name</label>'),
+					$('<input id="new-button-name" name="new-button-name" required min=1 />'),
+					$('<br />'),
+					$('<label for="new-button-text">Text</label>'),
+					$('<textarea id="new-button-text" name="new-button-text" required min=1></textarea>'),
+				],
+				"Add Button...",
+				(e: FormData) => {
+					const temp = this.Settings.buttons;
+					temp.push({
+						name: e.get("new-button-name").toString(),
+						text: e.get("new-button-text").toString(),
+					});
+					this.Settings.buttons = temp;
+					this.buildButtons();
+				},
+			);
+
+			// Stop propagation & prevent default.
+			return false;
+		});
+		this.removeButton.on("click", (e) => {
+			if (this.toggleState()) {
+				e.target.innerText = "x";
+				e.target.title = "Exit button removal mode.";
+				this.addButton.attr("disabled", "true");
+			} else {
+				e.target.innerText = "-";
+				e.target.title = "Enter button removal mode.";
+				this.addButton.removeAttr("disabled");
+			}
+
+			// Stop propagation & prevent default.
+			return false;
+		});
+		this.resetButton.on("click", () => {
+			if (confirm("Are you sure you want to reset the buttons to the defaults?\n\nThis will permanently remove your custom buttons.")) {
+				this.Settings.buttons = TicketReasons.createDefaultSettingsButtons();
+				this.buildButtons();
+			}
+
+			// Stop propagation & prevent default.
+			return false;
+		});
+	}
+
+	/**
+	 * This class doesn't work unless re621 has added the `#modal-container` element, so this manually ensures it's added to enable it to work w/o re621.
+	 */
+	private static ensureModalContainer() {
+		if (!document.querySelector("#modal-container")) {
+			console.log("%c[RE621.ModTools]%c: no re621 detected; manually creating 'div#modal-container'...", "color: maroon", "color: unset");
+			$("<div>").attr("id", "modal-container").prependTo("div#page");
+		}
+	}
 }
