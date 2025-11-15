241e4614dae4c935b057f4a01e0f13163e3a796e
Justin Morris <jmortiger@gmail.com>
2025-11-14 20:20:42 -0500

    [DMail] Custom Staff Note Template

--- src/js/components/DMailToStaffNote.ts
+++ src/js/components/DMailToStaffNote.ts
@@ -2,13 +2,25 @@ import { PageDefinition } from "../models/data/Page";
 import Component from "./Component";
 import REMT from "../../REMT";
 import Danbooru from "../models/api/Danbooru";
+import Util from "../utilities/Util";
+import { DialogForm } from "../models/structure/DialogForm";
 
 export default class DMailToStaffNote extends Component {
-	private dmailBodyText: undefined | string;
-	private dmailTitleText: undefined | string;
+	public static readonly TICKET_MATCHER = /^"Your ticket":\/tickets\/([0-9]+) has been updated by /;
+	private readonly templateVariables = new Map<string|RegExp, string|((key: string, ...args: any[]) => string)>([
+		["dmailId", () => this.dmailJson["id"].toString()],
+		["title", () => this.dmailJson["title"].toString()],
+		["ticketId", (v) => {
+			if (!this.dmailJson || !(this.dmailJson["body"])) return v; // `%${v}%`;
+			const b: string = this.dmailJson["body"].toString();
+			return DMailToStaffNote.TICKET_MATCHER.test(b) ? DMailToStaffNote.TICKET_MATCHER.exec(b)[1].toString() : v; // `%${v}%`;
+		}],
+	]);
 	private dmailJson: any | undefined;
 	/** Indicates if we're currently getting the DMail JSON. */
 	private isFetching = false;
+	/** Indicates if we've previously gotten the DMail JSON. */
+	private wasFetched = false;
 
 	public constructor() {
 		super({
@@ -17,12 +29,41 @@ export default class DMailToStaffNote extends Component {
 		});
 	}
 
-	// NOTE: Currently needs to get the body's raw DText from server.
-	// IDEA: Add option to edit a preexisting staff note.
-	// TODO: Disable textarea & reenable it when the submit button is pressed so it's actually included in the form's output.
+	public Settings = {
+		enabled: true,
+		header: '"DMail":[/dmails/%dmailId%] sent regarding ticket #%ticketId%\n[section=$title]',
+		footer: "[/section]\n",
+	};
+
+	private revealSettingsDialog(): boolean {
+		DialogForm.getRequestedInput(
+			[
+				$('<label for="setting-header">Default Staff Note Header</label>'),
+				$(`<textarea id="setting-header" name="setting-header"></textarea>`).text(this.Settings.header),
+				$('<br />'),
+				$('<label for="setting-footer">Default Staff Note Footer</label>'),
+				$(`<textarea id="setting-footer" name="setting-footer"></textarea>`).text(this.Settings.footer),
+			],
+			"Settings",
+			(e: FormData) => {
+				this.Settings.header = e.get("setting-header").toString();
+				this.Settings.footer = e.get("setting-footer").toString();
+				// TODO: Update text boxes
+			},
+		);
+		return false;
+	}
+
+	/**
+	 * NOTE: Currently needs to get the body's raw DText from server.
+	 * IDEA: Add option to edit a preexisting staff note.
+	 * @todo: Disable textarea & reenable it when the submit button is pressed so it's actually included in the form's output.
+	 */
 	protected create(): Promise<void> {
 		const dmailInfo = DMailToStaffNote.findDMailIds();
-		this.dmailTitleText = dmailInfo.title;
+		this.dmailJson = {};
+		this.dmailJson.id = dmailInfo.id;
+		this.dmailJson.title = dmailInfo.title;
 
 		const cBox = document.createElement("input"),
 			cBoxLabel = document.createElement("label"),
@@ -44,21 +85,26 @@ export default class DMailToStaffNote extends Component {
 			submit = document.createElement("input");
 
 		const updateStaffNoteBodyDisplay = () => {
-			staff_note_body.value = `${noteHeader.value.replace("$title", this.dmailTitleText || "")}\n${this.dmailBodyText || ""}\n${noteFooter.value}`;
+			// staff_note_body.value = `${Util.replaceTemplateVariables(noteHeader.value, this.templateVariables)}\n${this.dmailBodyText || ""}\n${Util.replaceTemplateVariables(noteFooter.value, this.templateVariables)}`;
+			staff_note_body.value = `${Util.replaceTemplateVariables(noteHeader.value, this.templateVariables)}\n${this.dmailJson["body"] || ""}\n${Util.replaceTemplateVariables(noteFooter.value, this.templateVariables)}`;
 			$(staff_note_body).trigger("input.danbooru.formatter");
 		}
 
 		const fetchDMailJSON = async () => {
 			const t = await (await fetch(`/dmails/${dmailInfo.id}.json`)).json();
 			this.dmailJson = t;
-			this.dmailTitleText = t["title"];
-			this.dmailBodyText = t["body"];
 			userDropdownRecipient.innerText = t["to_name"];
 			userDropdownSender.innerText = t["from_name"];
 			userDropdownRecipient.value = t["to_id"];
 			userDropdownSender.value = t["from_id"];
 			submit.disabled = false;
 			this.isFetching = false;
+			this.wasFetched = true;
+			// Update header/footer values
+			// IDEA: Somehow also move cursor accordingly if currently being edited?
+			noteHeader.value = Util.replaceTemplateVariables(noteHeader.value, this.templateVariables);
+			noteFooter.value = Util.replaceTemplateVariables(noteFooter.value, this.templateVariables);
+			// Update body
 			updateStaffNoteBodyDisplay();
 		};
 
@@ -120,7 +166,7 @@ export default class DMailToStaffNote extends Component {
 		// #region noteHeaderLabel & noteHeader
 		noteHeaderLabel.innerText = "Staff Note Header";
 		noteHeaderLabel.htmlFor = noteHeader.id = noteHeader.name = "note-header-input";
-		noteHeader.value = `"DMail":[/dmails/${dmailInfo.id}] sent regarding ticket #\n[section=$title]`;
+		noteHeader.value = Util.replaceTemplateVariables(this.Settings.header, this.templateVariables);
 		noteHeader.oninput = updateStaffNoteBodyDisplay;
 
 		noteHeader.style.width = "100%";
@@ -129,7 +175,7 @@ export default class DMailToStaffNote extends Component {
 		// #region noteFooterLabel & noteFooter
 		noteFooterLabel.innerText = "Staff Note Footer";
 		noteFooterLabel.htmlFor = noteFooter.id = noteFooter.name = "note-footer-input";
-		noteFooter.value = "[/section]\n";
+		noteFooter.value = Util.replaceTemplateVariables(this.Settings.footer, this.templateVariables);
 		noteFooter.oninput = updateStaffNoteBodyDisplay;
 
 		noteFooter.style.width = "100%";
@@ -143,7 +189,7 @@ export default class DMailToStaffNote extends Component {
 		cBox.onchange = () => {
 			if (cBox.checked) {
 				form.style.display = inputBox.style.display = "revert";
-				if (!this.dmailJson && !this.isFetching) {
+				if (!this.wasFetched && !this.isFetching) {
 					this.isFetching = true;
 					fetchDMailJSON();
 				} else {
@@ -157,6 +203,12 @@ export default class DMailToStaffNote extends Component {
 		const content = document.querySelector(".dmail");
 		content.insertAdjacentElement("beforeend", cBoxLabel);
 		content.insertAdjacentElement("beforeend", cBox);
+		// #region Settings Button
+		const settingsButton = document.createElement("button");
+		settingsButton.innerText = "Edit Settings";
+		settingsButton.onclick = () => this.revealSettingsDialog();
+		content.insertAdjacentElement("beforeend", settingsButton);
+		// #endregion Settings Button
 		content.insertAdjacentElement("beforeend", inputBox);
 		content.insertAdjacentElement("beforeend", form);
 		inputBox.appendChild(userDropdownLabel);
