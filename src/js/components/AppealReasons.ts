d35432a55ff7f55e268cd53b98d9ebb30c711daf
jmortiger <jmortiger@gmail.com>
2026-06-25 13:14:44 -0400

    Add support to change TemplateBuilder insertMode to consumers (#29)
    * Added support to change TemplateBuilder insertMode to consumers
    
    * Bump version, minor tweak

--- src/js/components/AppealReasons.ts
+++ src/js/components/AppealReasons.ts
@@ -1,6 +1,9 @@
 import { PageDefinition } from "../models/data/Page";
 import User from "../models/data/User";
+import { DialogForm } from "../models/structure/DialogForm";
 import { TemplateBuilder, TemplateData } from "../models/structure/TemplateBuilder";
+import { html } from "../utilities/HtmlTemplate";
+import Util from "../utilities/Util";
 import Component from "./Component";
 
 const DEFAULT_GREETING = "Hi %reporterName%,\n\n";
@@ -18,7 +21,7 @@ export default class AppealReasons extends Component {
 			waitForDOM: "textarea[name='appeal[response]']",
 			dependencies: [],
 		});
-		this.isEnabled = User.isJanitor ||  User.isModerator || User.isAdmin;
+		this.isEnabled = User.isJanitor || User.isModerator || User.isAdmin;
 	}
 
 	public static get defaultTemplates(): TemplateData[] {
@@ -28,24 +31,32 @@ export default class AppealReasons extends Component {
 		];
 	}
 
-	public Settings: { enabled: boolean; buttons: TemplateData[]; greeting: string } = {
+	public Settings: { enabled: boolean; buttons: TemplateData[]; insertMode: "replace" | "insert"; greeting: string; } = {
 		enabled: true,
 		buttons: AppealReasons.defaultTemplates,
+    insertMode: "replace",
 		greeting: DEFAULT_GREETING,
 	};
 
+  private readonly settingsButtonLabel = "Appeal Template Settings";
 	protected create(): Promise<void> {
 		if (!this.isEnabled) return Promise.resolve();
+    Util.DOM.addSettingsButton({
+      id: this.settingsButtonLabel.replace(/\s+/g, "-"),
+      name: this.settingsButtonLabel,
+      onClick: () => this.onSettingsButton(),
+    });
 
 		const target = document.querySelector<HTMLTextAreaElement>("textarea[name='appeal[response]']");
 		if (!target) return Promise.resolve();
 
 		this.reporterName = document.querySelector<HTMLElement>("#c-appeals .appeal-display-report-creator a")?.innerText ?? "";
 
+    const scopedInsertMode = () => this.Settings.insertMode;
 		this.builder = new TemplateBuilder({
 			targetField: target,
 			label: "Appeal reply templates",
-			insertMode: "replace",
+			get insertMode() { return scopedInsertMode(); },
 			defaults: AppealReasons.defaultTemplates,
 			getTemplates: () => this.Settings.buttons,
 			setTemplates: (next) => { this.Settings.buttons = next; },
@@ -70,4 +81,31 @@ export default class AppealReasons extends Component {
 	protected async destroy(): Promise<void> {
 		this.builder?.destroy();
 	}
+  
+  /**
+	 * The callback to execute when the settings button is pressed.
+	 * 
+	 * NOTE: Dependent on proper `this` binding; assign to events in a callback.
+	 * @returns false to stop propagation & prevent default.
+	 */
+  protected onSettingsButton(): false {
+    DialogForm.getRequestedInput(
+      [
+        $(html`<fieldset title="How should the button's text be added to the text box?">
+            <legend>Text insertion mode</legend>
+            <label for="setting-insertMode-insert" title="Insert the text at the cursor position.">Insert <input type="radio" id="setting-insertMode-insert" name="setting-insertMode" value="insert"${(this.Settings.insertMode ?? "insert") === "insert" ? " checked" : ""} /></label>
+            <label for="setting-insertMode-replace" title="Replace the entire contents of the text box.">Replace <input type="radio" id="setting-insertMode-replace" name="setting-insertMode" value="replace"${this.Settings.insertMode === "replace" ? " checked" : ""} /></label>
+          </fieldset>` as HTMLFieldSetElement),
+        $(`<br />`),
+      ],
+      this.settingsButtonLabel,
+      (e: FormData) => {
+        const v = e.get("setting-insertMode");
+        if (v && (v === "insert" || v === "replace")) this.Settings.insertMode = v;
+      },
+    );
+
+    // Stop propagation & prevent default.
+    return false;
+  }
 }
--- src/js/components/ForumBuilder.ts
+++ src/js/components/ForumBuilder.ts
@@ -1,14 +1,19 @@
 import { PageDefinition } from "../models/data/Page";
+import { DialogForm } from "../models/structure/DialogForm";
 import { TemplateBuilder, TemplateData } from "../models/structure/TemplateBuilder";
+import { html } from "../utilities/HtmlTemplate";
+import Util from "../utilities/Util";
 import Component from "./Component";
 
 export default class ForumBuilder extends Component {
-	public Settings: { enabled: boolean; buttons: TemplateData[] } = {
+	public Settings: { enabled: boolean; buttons: TemplateData[]; insertMode: "replace" | "insert"; } = {
 		enabled: true,
 		buttons: [],
+    insertMode: "insert",
 	};
 
 	private builder?: TemplateBuilder;
+	private builders?: TemplateBuilder[];
 
 	public constructor() {
 		super({
@@ -17,21 +22,63 @@ export default class ForumBuilder extends Component {
 		});
 	}
 
+  private readonly settingsButtonLabel = "Forum Template Settings";
 	protected create(): Promise<void> {
+    Util.DOM.addSettingsButton({
+      id: this.settingsButtonLabel.replace(/\s+/g, "-"),
+      name: this.settingsButtonLabel,
+      onClick: () => this.onSettingsButton(),
+    });
 		const target = document.querySelector<HTMLTextAreaElement>("form.new_forum_post textarea[name='forum_post[body]']");
 		if (!target) return Promise.resolve();
 
-		this.builder = new TemplateBuilder({
-			targetField: target,
-			label: "Forum templates",
-			getTemplates: () => this.Settings.buttons,
-			setTemplates: (next) => { this.Settings.buttons = next; },
-		});
-		this.builder.mount();
+		this.builder = this.instantiateAndMount(target);
+
+    this.builders = Array.from(document.querySelectorAll<HTMLTextAreaElement>('form.edit_forum_post textarea[name="forum_post[body]"]')).map(target => this.instantiateAndMount(target));
 		return Promise.resolve();
 	}
 
 	protected async destroy(): Promise<void> {
 		this.builder?.destroy();
+    this.builders?.forEach(e => e.destroy());
 	}
+
+  private instantiateAndMount(target: HTMLTextAreaElement, scopedInsertMode = () => this.Settings.insertMode) {
+    const builder = new TemplateBuilder({
+			targetField: target,
+			label: "Forum templates",
+      get insertMode() { return scopedInsertMode(); },
+			getTemplates: () => this.Settings.buttons,
+			setTemplates: (next) => { this.Settings.buttons = next; },
+		});
+		builder.mount();
+    return builder;
+  }
+  
+  /**
+	 * The callback to execute when the settings button is pressed.
+	 * 
+	 * NOTE: Dependent on proper `this` binding; assign to events in a callback.
+	 * @returns false to stop propagation & prevent default.
+	 */
+  protected onSettingsButton(): false {
+    DialogForm.getRequestedInput(
+      [
+        $(html`<fieldset title="How should the button's text be added to the text box?">
+            <legend>Text insertion mode</legend>
+            <label for="setting-insertMode-insert" title="Insert the text at the cursor position.">Insert <input type="radio" id="setting-insertMode-insert" name="setting-insertMode" value="insert"${(this.Settings.insertMode ?? "insert") === "insert" ? " checked" : ""} /></label>
+            <label for="setting-insertMode-replace" title="Replace the entire contents of the text box.">Replace <input type="radio" id="setting-insertMode-replace" name="setting-insertMode" value="replace"${this.Settings.insertMode === "replace" ? " checked" : ""} /></label>
+          </fieldset>` as HTMLFieldSetElement),
+        $(`<br />`),
+      ],
+      this.settingsButtonLabel,
+      (e: FormData) => {
+        const v = e.get("setting-insertMode");
+        if (v && (v === "insert" || v === "replace")) this.Settings.insertMode = v;
+      },
+    );
+
+    // Stop propagation & prevent default.
+    return false;
+  }
 }
