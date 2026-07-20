b9b186936739b88d57f941b2e9de7e161fc2ae57
J Mor <jmortiger@gmail.com>
2026-07-20 12:45:22 -0400

    Made config more data-driven The domains, runtime dependency version numbers, & more are now derived from a universal source of truth, making downstream repositories for other sites more manageable & consistent.

--- src/js/models/data/Script.ts
+++ src/js/models/data/Script.ts
@@ -1,14 +1,16 @@
+import { UtilNetwork } from '../../utilities/UtilNetwork';
 /**
  * Basic script parameters, both hard-coded and derived from the userscript header.
  * GM_info is used here because of XM.Connect initialization issues.
  */
 export default class Script {
-  public static version = GM_info.script.version;
-  public static handler = {
+  public static readonly displayName = GM_info.script.name;
+  public static readonly version = GM_info.script.version;
+  public static readonly handler = Object.freeze({
     name: GM_info.scriptHandler,
     version: GM_info.version,
-  }
-  public static url = {
+  });
+  public static readonly url = {
     website: "https://re621.app",
     repo: "https://github.com/re621/re621.ModTools",
     issues: "https://github.com/re621/re621.ModTools/issues",
@@ -16,11 +18,34 @@ export default class Script {
     latest: "https://api.github.com/repos/re621/re621.ModTools/releases/latest",
     kofi: "https://ko-fi.com/bitWolfy",
   }
-  public static userAgent = "re621.ModTools/" + this.trimVersion(Script.version);
 
+  /**
+   * Changes the script's name to match the pre-established User Agent format.
+   * E.g. If the package's name (& consequently script's name) is `RE621 Mod Tools`, that becomes `re621.ModTools`.
+   * @deprecated Use {@link UtilNetwork.projectNameFormatted} instead.
+   */
+  private static projectNameFormatted = this.displayName.split(" ").reduce((p, e) => p + (p ? e.toLowerCase().replace(/^./, (e) => e.toUpperCase()) : e.toLowerCase() + "."), "");
+
+  /**
+   * 
+   * @deprecated Use {@link UtilNetwork.userAgent} instead.
+   */
+  public static readonly userAgent = this.projectNameFormatted + this.trimVersion(Script.version);
+
+  /**
+   * @deprecated Use {@link UtilNetwork.trimVersion} instead
+   */
   private static trimVersion(value: string): string {
     const match = value.match(/(\d\.\d+)\.\d+/);
     if (!match || !match[1]) return "0.0";
     return match[1];
   }
+
+  public static readonly domains = GM_info.script.matches.map(e => e.match(/^https:\/\/(.+)\/\*/)?.[1]).reduce<string[]>((p,e) => {
+    if (e) p.push(e);
+    return p;
+  }, []);
+  public static readonly primaryDomain = this.domains[0] ?? "e621.net";
+  public static readonly staticDomains = GM_info.script.options.override.orig_connects.filter(e => e && e !== "*");
+  public static readonly primaryStaticDomain = this.staticDomains[0] ?? `static1.${this.primaryDomain}`;
 }
