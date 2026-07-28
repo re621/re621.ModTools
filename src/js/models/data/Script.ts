3e5e7e5a9525dc79b9d8ea9bb0df3c7b90688b96
J Mor <jmortiger@gmail.com>
2026-07-28 13:26:55 -0400

    Expanded `Script`'s project config members

--- src/js/models/data/Script.ts
+++ src/js/models/data/Script.ts
@@ -1,4 +1,5 @@
-import { UtilNetwork } from '../../utilities/UtilNetwork';
+import Util from '../../utilities/Util';
+
 /**
  * Basic script parameters (both hard-coded and derived from the userscript
  * header), & miscellaneous site-specific configuration.
@@ -9,38 +10,70 @@ import { UtilNetwork } from '../../utilities/UtilNetwork';
  * * `GM_info.script.name`: At least 2 space-separated terms.
  * * `GM_info.script.version`: A proper SemVer version; can have any prefix.
  * * `GM_info.script.homepage`: A proper GitHub repository URL.
+ *
+ * # IMPORTANT
+ * If this is modified to utilize external resources from this library (e.g. the use of {@link Util.Network.normalizeUrl}), make certain to account for circular dependencies (e.g. {@link Util.Network.userAgent} is lazily initialized).
  */
 export default class Script {
   public static readonly displayName = GM_info.script.name;
+  /**
+   * The raw version; expected to contain a
+   * [SemVer](https://semver.org/)-formatted string.
+   *
+   * @deprecated Will be replaced with {@link versionObj} next major release; use {@link versionObj.raw} instead.
+   * @todo Replace w/ copy of `versionObj`; deprecate current `versionObj`.
+   */
   public static readonly version = GM_info.script.version;
+  /**
+   * An object containing the parsed version info.
+   */
+  public static readonly versionObj = Object.freeze(Util.parseVersion(this.version));
   public static readonly handler = Object.freeze({
     name: GM_info.scriptHandler,
     version: GM_info.version,
   });
   public static readonly url = {
     website: "https://re621.app",
-    repo: "https://github.com/re621/re621.ModTools",
-    issues: "https://github.com/re621/re621.ModTools/issues",
+    repo: GM_info.script.homepage ? Util.Network.normalizeUrl(GM_info.script.homepage) : "~~HOMEPAGE_MISSING~~",
+    issues: (GM_info.script.homepage ? Util.Network.normalizeUrl(GM_info.script.homepage) : "~~HOMEPAGE_MISSING~~") + "/issues",
     thread: "https://e621.net/forum_topics/25872",
     latest: "https://api.github.com/repos/re621/re621.ModTools/releases/latest",
     kofi: "https://ko-fi.com/bitWolfy",
   }
 
+  // #region Internal Project Identifiers
+  /**
+   * Changes the script's name to match the pre-established console logging format.
+   *
+   * E.g. If the package's name (& consequently script's name) is `RE621 Mod
+   * Tools`, that becomes `RE621.ModTools`.
+   */
+  public static readonly projectNameFormatted = Script.displayName.split(" ").reduce((p, e) => p + (p ? e.toLowerCase().replace(/^./, e => e.toUpperCase()) : e + "."), "");
+
   /**
    * Changes the script's name to match the pre-established User Agent format.
-   * E.g. If the package's name (& consequently script's name) is `RE621 Mod Tools`, that becomes `re621.ModTools`.
-   * @deprecated Use {@link UtilNetwork.projectNameFormatted} instead.
+   *
+   * E.g. If the package's name (& consequently script's name) is `RE621 Mod
+   * Tools`, that becomes `re621.ModTools`.
    */
-  private static projectNameFormatted = this.displayName.split(" ").reduce((p, e) => p + (p ? e.toLowerCase().replace(/^./, (e) => e.toUpperCase()) : e.toLowerCase() + "."), "");
+  public static readonly projectNameCamelFormatted = Script.displayName.split(" ").reduce((p, e) => p + (p ? e.toLowerCase().replace(/^./, e => e.toUpperCase()) : e.toLowerCase() + "."), "");
 
+  /**
+   * Changes the script's name to match an old pre-established event prefix format.
+   *
+   * E.g. If the package's name (& consequently script's name) is `RE621 Mod
+   * Tools`, that becomes `re621-mt`.
+   */
+  public static readonly eventPrefixLegacy = Script.displayName.split(" ").reduce((p, e) => p + (p ? e[0].toLowerCase() : e.toLowerCase() + "-"), "");
+  // #endregion Internal Project Identifiers
   /**
    * 
-   * @deprecated Use {@link UtilNetwork.userAgent} instead.
+   * @deprecated Use {@link Util.Network.userAgent} instead.
    */
-  public static readonly userAgent = this.projectNameFormatted + this.trimVersion(Script.version);
+  public static readonly userAgent = this.projectNameFormatted + this.trimVersion(Script.versionObj.raw);
 
   /**
-   * @deprecated Use {@link UtilNetwork.trimVersion} instead
+   * @deprecated Use {@link Util.parseVersion} instead
    */
   private static trimVersion(value: string): string {
     const match = value.match(/(\d\.\d+)\.\d+/);
@@ -48,11 +81,45 @@ export default class Script {
     return match[1];
   }
 
-  public static readonly domains = GM_info.script.matches.map(e => e.match(/^https:\/\/(.+)\/\*/)?.[1]).reduce<string[]>((p,e) => {
-    if (e) p.push(e);
-    return p;
-  }, []);
+  // #region Domains
+  /** E.g. For e621, should be `["e621.net", "e926.net"]`. */
+  public static readonly domains = Object.freeze(
+    GM_info
+      .script
+      .matches
+      .map(e => e.match(/^https:\/\/(.+)\/\*/)?.[1]).reduce<string[]>((p,e) => {
+        if (e) p.push(e);
+        return p;
+      }, []),
+  );
   public static readonly primaryDomain = this.domains[0] ?? "e621.net";
-  public static readonly staticDomains = GM_info.script.options.override.orig_connects.filter(e => e && e !== "*");
+
+  /** E.g. for e621, should be `["static1.e621.net", "static1.e926.net", "*"]` */
+  public static readonly staticDomains = Object.freeze(
+    GM_info.script.options.override.orig_connects.filter(e => e && e !== "*"),
+  );
   public static readonly primaryStaticDomain = this.staticDomains[0] ?? `static1.${this.primaryDomain}`;
+
+  /** E.g. For e621, should be `["e621", "e926"]`. */
+  public static readonly secondLevelDomains = Object.freeze(
+    this.domains.map(e => e.split(".").at(-2) ?? e.split(".")[0] ?? e),
+  );
+  public static readonly primarySecondLevelDomain = this.secondLevelDomains[0] ?? "e621";
+
+  /**
+   * What do important wiki pages have as their prefix by convention?
+   *
+   * E.g. For e621 & e926, `e621` (as used for `[[e621:rules]]`, et. al.).
+   */
+  public static readonly siteNameWikiPrefix = this.primarySecondLevelDomain;
+
+  // #region Misc
+  /**
+   * Used to reference the name of the companion userscript; should one exist.
+   *
+   * This project started as a slimmer version of re621; this is used for log
+   * messages related to its expected presence or lack thereof.
+   */
+  public static readonly companionProjectName = "re621";
+  // #endregion Misc
 }
